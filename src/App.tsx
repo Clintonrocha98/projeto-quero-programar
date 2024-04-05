1b7f856be3c60feb5af3d5ceaae3d7ff7d68beca
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-04-05 09:45:17 -0300

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,149 @@
+function App() {
+  return (
+    <main className="max-w-sm md:max-w-3xl lg:max-w-5xl px-7 ml-auto mr-auto flex flex-col gap-5 mt-4">
+      <section>
+        <h2>O BASICO PARA TUDO DA PROGRAMAÇÃO</h2>
+        <ul>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=8mei6uVttho&list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV"
+              target="_blank"
+            >
+              Curso de logica de programação e algoritmo
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=xEKo29OWILE&list=PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA"
+              target="_blank"
+            >
+              Curso de git e github.
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/DanielHe4rt/git4noobs" target="_blank">
+              Leitura: git4noobs
+            </a>
+          </li>
+        </ul>
+
+        <h2>Quer fazer site? aprenda isso aqui</h2>
+        <ul>
+          <li>
+            Leitura:{" "}
+            <a
+              href="https://github.com/ThiagoDellaNoce/javascript4noobs"
+              target="_blank"
+            >
+              javascript4noobs
+            </a>
+          </li>
+          <li>
+            Curso:{" "}
+            <a
+              href="https://github.com/ThiagoDellaNoce/javascript4noobs"
+              target="_blank"
+            >
+              javascript 40 horas
+            </a>
+          </li>
+          <li>
+            Conteudo auxiliar css:{" "}
+            <a href="https://github.com/he4rt/css4noobs" target="_blank">
+              css4noobs
+            </a>
+          </li>
+          <li>
+            Conteudo auxiliar html:{" "}
+            <a href="https://github.com/lucassoren/HTML4Noobs" target="_blank">
+              html4noobs
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=Ejkb_YpuHWs&list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n"
+              target="_blank"
+            >
+              html/css - modulo 1
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=vPNIAJ9B4hg&list=PLHz_AreHm4dlUpEXkY1AyVLQGcpSgVF8s"
+              target="_blank"
+            >
+              html/css - modulo 2
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=ofFgnDtn_1c&list=PLHz_AreHm4dmcAviDwiGgHbeEJToxbOpZ"
+              target="_blank"
+            >
+              html/css - modulo 3
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=zHKHMmEG9vE&list=PLHz_AreHm4dkcVCk2Bn_fdVQ81Fkrh6WT"
+              target="_blank"
+            >
+              html/css - modulo 4
+            </a>
+          </li>
+        </ul>
+
+        <h2>Roteiros de estudos para não se sentir perdido</h2>
+        <ul>
+          <li>
+            <a href="https://roadmap.sh" target="_blank">
+              Roadmap.sh
+            </a>
+          </li>
+          <li>
+            <a href="https://www.trilha.info" target="_blank">
+              Trilha.info
+            </a>
+          </li>
+          <li>
+            <a href="https://techguide.sh" target="_blank">
+              Techguide.sh
+            </a>
+          </li>
+        </ul>
+
+        <h2>LEIA ISSO AQUI</h2>
+        <ul>
+          <li>
+            <a
+              href="https://felipperegazio.com/posts/o-minimo-necessario-js-para-frameworks/"
+              target="_blank"
+            >
+              O mínimo que vc precisa saber de JavaScript pra que seja
+              confortável codar e aprender qualquer Framework Front End
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://felipperegazio.com/posts/12-dicas-nao-tecnicas-primeiro-emprego/"
+              target="_blank"
+            >
+              12 Dicas NÃO TECNICAS para você que está buscando seu Primeiro
+              Emprego na área de Desenvolvimento de Software
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://felipperegazio.com/posts/entao-vc-quer-ser-dev/"
+              target="_blank"
+            >
+              Quero ser dev
+            </a>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default App;
