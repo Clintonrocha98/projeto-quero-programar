dea46a24babd7499718ca766160e5a17950e084a
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-05-04 21:44:36 -0300

    v3

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <script defer src='https://static.cloudflareinsights.com/beacon.min.js'
-      data-cf-beacon='{"token": "fe1b0c057be24681a9a3c0134775eb76"}'></script>
   </React.StrictMode>
 );
